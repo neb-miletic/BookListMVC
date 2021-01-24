dde37eb8d86300ba839c5f269e0f44383bcf3979
neb-miletic <30331783+nebTheWeb@users.noreply.github.com>
2021-01-24 17:04:08 +1100

    upsert

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,91 @@ namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+
+
+        private readonly ApplicationDbContext _db;
+
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    //create
+                    _db.Books.Add(Book);
+                }
+
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                //create
+                return View(Book);
+            }
+
+            //update
+            Book = _db.Books.FirstOrDefault(u => u.Id == 0);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return View(Book);
+        }
+        #region API Calls
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error whle deleting" });
+            }
+
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+
+
+        #endregion
+
+
     }
 }
